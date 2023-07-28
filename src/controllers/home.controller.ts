45c58beb5179bcca55d78d39a9b5821311c9f507
papontron <jaracristhiam@gmail.com>
2023-07-27 22:06:03 -0500

    title

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+const keys = require('../keys');
 export default function homeController(req: Request, res: Response) {
-  res.render('index');
+  res.render('index', { port: keys.port });
 }
